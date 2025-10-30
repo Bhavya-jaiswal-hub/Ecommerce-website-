2100760d6ec300796ba8906018e17ac06c872043
bhavya <bhavya2313139@akgec.ac.in>
2025-10-30 18:45:15 +0530

    do the work on the prouduct page

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -13,7 +13,7 @@ const SearchBar = () => {
      const location = useLocation();
 
      useEffect(() => {
-         if(location.pathname.includes('collection') && showSearch) {
+         if(location.pathname.includes('collection') ) {
              setVisible(true);
          } 
 
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -39,6 +39,10 @@ const Collection = () => {
   const applyFilter = () => {
      let productsCopy = products.slice();
 
+     if(showSearch && search) {
+       productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+     }
+
      if(category.length > 0) {
        productsCopy = productsCopy.filter(item => category.includes(item.category));
      }
@@ -79,7 +83,7 @@ const Collection = () => {
 
   useEffect(() => {
      applyFilter();
-  }, [category,subcategory])
+  }, [category,subcategory,search,showSearch])
 
   useEffect(() => {
     sortProduct();
