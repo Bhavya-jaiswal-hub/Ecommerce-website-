2607b4b72e9906773e02ddaf90b790ec41947a43
bhavya <bhavya2313139@akgec.ac.in>
2025-11-13 01:19:51 +0530

    added the  backend added the register authentication

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 // App Config
 const app = express()
@@ -9,12 +11,15 @@ const port = process.env.PORT || 4000
 
 
 connectDB()
+connectCloudinary()
 
 // Middlewares 
 app.use(express.json())
 app.use(cors())
 
 // API endpoints
+app.use('/api/user',userRouter)
+
 app.get('/', (req, res) => {
   res.send("API working")
 })
