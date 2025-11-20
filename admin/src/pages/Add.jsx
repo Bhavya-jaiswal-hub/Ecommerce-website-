4231b1b5e24f5c915ce1aaa4f4bd681e6276775e
bhavya <bhavya2313139@akgec.ac.in>
2025-11-20 20:23:10 +0530

    added the add items feature on the admin panel

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import {backendUrl} from '../App' 
+import {toast} from 'react-toastify'
 
-const Add = () => {  
+
+
+const Add = ({token}) => {  
     
  const [image1, setImage1]  = useState(false)
  const [image2, setImage2] =  useState(false) 
@@ -33,17 +38,34 @@ const Add = () => {
              image1 && formData.append("image1", image1)
                image2 && formData.append("image2", image2)
                  image3 && formData.append("image3", image3)
-                   image4 && formData.append("image4", image4)
+                   image4 && formData.append("image4", image4)  
 
+
+           const response = await axios.post(backendUrl + "/api/product/add" , formData,{headers:{token}}) 
+           
+           if(response.data.success) {
+             toast.success(response.data.message) 
+             setName('')
+             setDescription('')
+             setImage1(false)
+             setImage2(false)
+             setImage3(false)
+             setImage4(false)
+             setPrice('')
+           } 
+           else {
+             toast.error(response.data.message)
+           }
 
       }  catch (error) {
-         
+         console.log(error)
+         toast.error(error.message)
       }
 
     }
 
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3'>
      <div>
         <p className='mb-2'>
             Upload Image
