2310f55880aafcb942ff78200efe9e76aa4a2acc
bhavya <bhavya2313139@akgec.ac.in>
2025-12-13 14:09:57 +0530

    updated the server.js file

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,36 @@ import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoutes.js'
 import orderRouter from './routes/orderRoute.js'
 
-// App Config
 const app = express()
 const port = process.env.PORT || 4000
-
 
 connectDB()
 connectCloudinary()
 
-// Middlewares 
+// Middlewares
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: [
+    "https://zeestyle.in",
+    "https://www.zeestyle.in"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}))
+
+app.options("*", cors())
 
 // API endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product' , productRouter)
-app.use('/api/cart'  , cartRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 app.use('/api/order', orderRouter)
+
 app.get('/', (req, res) => {
   res.send("API working")
 })
 
-app.listen(port, () => console.log('Server Started on PORT : ' + port))
+app.listen(port, () =>
+  console.log('Server Started on PORT : ' + port)
+)
